723c915106b18ad93f9754f20a1ac675cf1eb1c9
IvanDimovSIT <ivan390@abv.bg>
2023-03-27 20:30:53 +0300

    Removed unused methods

--- src/models/ShoeOrder.ts
+++ src/models/ShoeOrder.ts
@@ -9,27 +9,4 @@ export class ShoeOrder{
         this.shoeSize = shoeSize;
     }
 
-    private stringToHash(str:string) {
-             
-        let hash:number = 0;
-         
-        if (str.length == 0) return hash;
-         
-        for (let i:number = 0; i < str.length; i++) {
-            let char:number = str.charCodeAt(i);
-            hash = ((hash << 5) - hash) + char;
-            hash = hash & hash;
-        }
-         
-        return hash;
-    }
-
-    equals(other: ShoeOrder): boolean {
-        return this.shoeItem.getId() === other.shoeItem.getId() && this.shoeSize === other.shoeSize;
-    }
-
-    hashCode(): number {
-        return this.shoeItem.getId() ^ this.stringToHash(this.shoeSize);
-    }
-
 }
