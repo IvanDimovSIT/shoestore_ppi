aa8924bee592565ff07828a64d8e4a41e1ca7671
IvanDimovSIT <ivan390@abv.bg>
2023-03-30 11:15:57 +0300

    Added ShoeItem gender description

--- src/models/ShoeItem.ts
+++ src/models/ShoeItem.ts
@@ -5,13 +5,15 @@ export class ShoeItem{
     private price:number;
     private picturePath:string;
     private gender:string;
+    private sizes:string[];
 
-    public constructor(id:number, name:string, price:number, picturePath:string, gender:string){
+    public constructor(id:number, name:string, price:number, picturePath:string, gender:string, sizes:string[]){
         this.id = id;
         this.name = name;
         this.price = price;
         this.picturePath = picturePath;
         this.gender = gender;
+        this.sizes = sizes;
     }
 
     public getId():number{
@@ -20,6 +22,10 @@ export class ShoeItem{
 
     public getName():string{
         return this.name;
+    }
+
+    public getSizes():string[]{
+        return this.sizes;
     }
 
     public getPrice():number{
@@ -32,6 +38,21 @@ export class ShoeItem{
 
     public getGender():string{
         return this.gender;
+    }
+
+    public getGenderDescription():string{
+        switch(this.gender)
+        {
+            case "M":
+                return "Men's Shoes";
+            case "F":
+                return "Women's Shoes";
+            case "K":
+                return "Kids' Shoes";
+            default:
+                console.error("Unrecogniesd Shoe gender:",this.gender, " from ", this);
+                return "ERROR";
+        }
     }
 
 }
