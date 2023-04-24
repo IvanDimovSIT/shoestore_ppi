db5e613e4ebcb70e1b01016ba04d9e0f5ac5817c
yordanov0502 <yordanovtodor281@gmail.com>
2023-04-24 19:08:42 +0300

    Added notifications to profile edit form

--- src/app/edit-profile-form/edit-profile-form.component.ts
+++ src/app/edit-profile-form/edit-profile-form.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { CurrentUser } from 'src/model/CurrentUser';
 import { User } from 'src/model/User';
 import { UserData } from 'src/model/UserData';
+import { NotificationComponent } from '../notification/notification.component';
 
 @Component({
   selector: 'app-edit-profile-form',
@@ -10,6 +11,7 @@ import { UserData } from 'src/model/UserData';
   styleUrls: ['./edit-profile-form.component.css']
 })
 export class EditProfileFormComponent {
+  @Input() public notification: NotificationComponent = new NotificationComponent();
   public firstName:string = "";
   public lastName:string = "";
   public phone:string = "";
@@ -32,6 +34,12 @@ export class EditProfileFormComponent {
     this.email = user.email;
   }
 
+  private showNotification(message: string): void {
+    console.log(message);
+    this.notification.message = message;
+    this.notification.showMessage();
+  }
+
   public update(){
     if(CurrentUser.email === null)
       return;
@@ -45,6 +53,7 @@ export class EditProfileFormComponent {
     user.phoneNumber = this.phone;
 
     UserData.set(user);
+    this.showNotification('Успешно редактиране!');
   }
 
   public navigateTo(path: string){
