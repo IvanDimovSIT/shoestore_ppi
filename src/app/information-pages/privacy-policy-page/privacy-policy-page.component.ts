d31bb29fce0d7e3752ef1ddd8a0b3d482967f9f7
IvanDimovSIT <ivan390@abv.bg>
2023-04-26 20:53:20 +0300

    Updated PrivacyPolicyPageComponent

--- src/app/information-pages/privacy-policy-page/privacy-policy-page.component.ts
+++ src/app/information-pages/privacy-policy-page/privacy-policy-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { BasePageComponent } from 'src/app/base-page/base-page.component';
 
 @Component({
   selector: 'app-privacy-policy-page',
   templateUrl: './privacy-policy-page.component.html',
   styleUrls: ['./privacy-policy-page.component.css']
 })
-export class PrivacyPolicyPageComponent {
-
+export class PrivacyPolicyPageComponent extends BasePageComponent{
 }
